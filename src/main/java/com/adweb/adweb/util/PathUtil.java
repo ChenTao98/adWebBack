adcc1c7ec485b2b8ff5af02eae28e61d0c693ca4
zhaoyangyingmu <16302010029@fudan.edu.cn>
2019-06-20 20:51:01 +0800

    safety commit.

--- src/main/java/com/adweb/adweb/util/PathUtil.java
+++ src/main/java/com/adweb/adweb/util/PathUtil.java
@@ -3,4 +3,6 @@ package com.adweb.adweb.util;
 public class PathUtil {
     public static final String COURSE_IMAGE_UP="F:\\adweb\\image\\course\\";
     public static final String COURSE_IMAGE_HTML="http://localhost:8080/image/course/";
+    public static final String TEACHER_IMAGE_UP = "F:\\adweb\\image\\teacher\\";
+    public static final String TEACHER_IMAGE_HTML = "http://localhost:8080/image/teacher";
 }
