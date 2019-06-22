6332b1a9cee506590f81c15329c2a22a78f606fe
zhaoyangyingmu <16302010029@fudan.edu.cn>
2019-06-22 21:31:38 +0800

    safe commit

--- src/main/java/com/adweb/adweb/util/PathUtil.java
+++ src/main/java/com/adweb/adweb/util/PathUtil.java
@@ -2,15 +2,15 @@ package com.adweb.adweb.util;
 
 public class PathUtil {
     // 开发环境
-    public static final String COURSE_IMAGE_UP="F:\\adweb\\image\\course\\";
-    public static final String TEACHER_IMAGE_UP = "F:\\adweb\\image\\teacher\\";
-    public static final String COURSE_IMAGE_HTML="http://localhost:18080/image/course/";
-    public static final String TEACHER_IMAGE_HTML = "http://localhost:18080/image/teacher/";
+//    public static final String COURSE_IMAGE_UP="F:\\adweb\\image\\course\\";
+//    public static final String TEACHER_IMAGE_UP = "F:\\adweb\\image\\teacher\\";
+//    public static final String COURSE_IMAGE_HTML="http://localhost:18080/image/course/";
+//    public static final String TEACHER_IMAGE_HTML = "http://localhost:18080/image/teacher/";
 
     // 部署环境
-//    public static final String COURSE_IMAGE_UP="/root/ADCourse/static/image/course/";
-//    public static final String TEACHER_IMAGE_UP = "/root/ADCourse/static/image/teacher/";
-//
-//    public static final String COURSE_IMAGE_HTML="http://47.102.201.65:18080/image/course/";
-//    public static final String TEACHER_IMAGE_HTML = "http://47.102.201.65:18080/image/teacher/";
+    public static final String COURSE_IMAGE_UP="/root/ADCourse/static/image/course/";
+    public static final String TEACHER_IMAGE_UP = "/root/ADCourse/static/image/teacher/";
+
+    public static final String COURSE_IMAGE_HTML="http://47.102.201.65:18080/image/course/";
+    public static final String TEACHER_IMAGE_HTML = "http://47.102.201.65:18080/image/teacher/";
 }
